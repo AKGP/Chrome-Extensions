231cf822f7dcdc3a33d24459032b4929b9cb9c5f
Ajay <ajay720945@gmail.com>
2016-10-28 13:19:06 +0530

    fix the tab issue

--- first/remove-intern.js
+++ first/remove-intern.js
@@ -3,3 +3,7 @@ obj[0].style.display = "none";
 obj[0].remove();
 var obj2 = document.getElementsByClassName("tab-head");
 obj2[0].className +=" active";
+var obj3 = document.getElementById("internships");
+var obj4 = document.getElementById("placements");
+obj3.remove();
+obj4.className+=" in active";
